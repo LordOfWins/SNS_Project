992ec5712f70115f41f7ab1e66abe082a5bd7191
윤승재 <sjyun011278@gmail.com>
2023-10-06 22:17:13 +0900

    feat: Call Bean #4

--- src/main/java/com/spring/spring/HelloWorldConfiguation.java
+++ src/main/java/com/spring/spring/HelloWorldConfiguation.java
@@ -7,7 +7,44 @@ import org.springframework.context.annotation.Configuration;
 public class HelloWorldConfiguation {
 
     @Bean
-    public String test() {
+    public String name() {
         return "World";
+    }
+
+    @Bean
+    public int age() {
+        return 30;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Seungjae", 27, new Address("I", "Seoul"));
+    }
+
+    @Bean
+    public Person personMethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person personParameter(String name, int age, Address address2) {
+        return new Person(name, age, address2);
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("I", "Seoul");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("K", "Busan");
+    }
+
+    public record Person(String name, int age, Address address) {
+    }
+
+
+    public record Address(String street, String city) {
     }
 }
--- src/main/java/com/spring/spring/HelloWorldSpring.java
+++ src/main/java/com/spring/spring/HelloWorldSpring.java
@@ -1,12 +1,30 @@
 package com.spring.spring;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class HelloWorldSpring {
+
+    public static final Log log = LogFactory.getLog(HelloWorldSpring.class);
+
     public static void main(String[] args) {
         //Launch a Spring context
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguation.class);
         //Configure the things that we want Spring to manage
-        System.out.println(context.getBean("test"));
+        log.info(context.getBean("name"));
+
+        log.info(context.getBean("age"));
+
+        log.info(context.getBean("person"));
+
+        log.info(context.getBean("personMethodCall"));
+
+        log.info(context.getBean("personParameter"));
+
+        log.info(context.getBean("address2"));
+
+//        log.info(context.getBean(HelloWorldConfiguation.Address.class));
     }
 }
