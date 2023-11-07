5616c496de0e688b2e45d70ecd1329c71bb99b16
윤승재 <sjyun011278@gmail.com>
2023-11-07 19:20:36 +0900

    feat: Connecting with H2 Database #36

--- webapp/src/main/java/com/springstudy/webapp/security/SpringSecurityConfiguration.java
+++ webapp/src/main/java/com/springstudy/webapp/security/SpringSecurityConfiguration.java
@@ -3,11 +3,15 @@ package com.springstudy.webapp.security;
 import java.util.function.Function;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -34,5 +38,22 @@ public class SpringSecurityConfiguration {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  // 기본적으로 설정되는 두 가지 값
+  // 모든 URL은 보호됨
+  // 인가 받지 않은 요청은 로그인 폼이 뜸
+  // 사이트간 요청 변조 CSRF를 비활성화
+  // 프레임 활성화
+
+  // CSRF
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+    http.formLogin(Customizer.withDefaults());
+    http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**").disable());
+    http.headers(
+        HttpHeaders -> HttpHeaders.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+    return http.build();
   }
 }
