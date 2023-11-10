b3dbfb254e619a079bfece1ba268591dc7e9b8a6
윤승재 <sjyun011278@gmail.com>
2023-11-10 22:27:15 +0900

    feat: Init Project #39

---
+++ Restful-web/src/main/java/org/springstudy/restfulweb/RestfulWebApplication.java
@@ -0,0 +1,12 @@
+package org.springstudy.restfulweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestfulWebApplication.class, args);
+  }
+}
