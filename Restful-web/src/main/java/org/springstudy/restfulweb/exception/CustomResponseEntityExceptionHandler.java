00a605a8f73c3b773e8c58c7766b51afee30f189
윤승재 <sjyun011278@gmail.com>
2023-11-12 19:17:21 +0900

    feat: Validation customizing #43

--- Restful-web/src/main/java/org/springstudy/restfulweb/exception/CustomResponseEntityExceptionHandler.java
+++ Restful-web/src/main/java/org/springstudy/restfulweb/exception/CustomResponseEntityExceptionHandler.java
@@ -1,8 +1,12 @@
 package org.springstudy.restfulweb.exception;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -24,5 +28,23 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     ErrorDetails errorDetails =
         new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
     return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    ErrorDetails errorDetails =
+        new ErrorDetails(
+            LocalDateTime.now(),
+            "Total Error : "
+                + ex.getErrorCount()
+                + " First Error : "
+                + Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(),
+            request.getDescription(false));
+
+    return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
   }
 }
