20bf68895a57d1c200a22348ce50e7eb870196a2
윤승재 <sjyun011278@gmail.com>
2023-11-13 19:38:21 +0900

    feat: Rest API 국제회 #46

--- Restful-web/src/main/java/org/springstudy/restfulweb/helloworld/HelloWorldController.java
+++ Restful-web/src/main/java/org/springstudy/restfulweb/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package org.springstudy.restfulweb.helloworld;
 
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+  private final MessageSource messageSource;
+
+  public HelloWorldController(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
 
   @GetMapping(path = "/hello")
   public String hello() {
@@ -20,5 +29,11 @@ public class HelloWorldController {
   @GetMapping(path = "/hello-world/path-variable/{name}")
   public HelloWorld helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorld(String.format("Hello World, %s!", name));
+  }
+
+  @GetMapping(path = "/hello-international")
+  public String helloInternational() {
+    Locale locale = LocaleContextHolder.getLocale();
+    return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
   }
 }
